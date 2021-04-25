a0752433d53997b03a7f52eef70ded768f573d00
Raphaëll <raphaelle.mp@gmail.com>
2021-04-25 17:38:33 -0400

    Les magiciens done

---
+++ Assets/PeopleMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleMove : MonoBehaviour
+{
+    public Vector3 positionToMoveTo;
+    public float duration;
+    private Vector3 startPosition;
+    bool forth = false;
+
+    void Start() {
+        startPosition = this.transform.position;
+        StartCoroutine(LerpPosition(positionToMoveTo, duration));
+    }
+
+    IEnumerator LerpPosition(Vector3 targetPosition, float duration) {
+        float time = 0;
+        Vector3 startPosition = transform.position;
+
+        while (time < duration) {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+        forth = !forth;
+        if(forth) StartCoroutine(LerpPosition(startPosition, duration));
+        else StartCoroutine(LerpPosition(positionToMoveTo, duration));
+    }
+}
